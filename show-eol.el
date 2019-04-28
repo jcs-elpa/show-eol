936b1e0ab1ed706359a1b0c6b222eb65ea612366
jenchieh <jcs090218@gmail.com>
2019-04-28 22:38:36 +0800

    Add core.

---
+++ show-eol.el
@@ -0,0 +1,38 @@
+;;; show-eol.el --- Show end of line in buffer.  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2019  Shen, Jen-Chieh
+;; Created date 2019-04-28 22:34:40
+
+;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
+;; Description: Show end of line in buffer.
+;; Keyword: end eol line
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "24.3"))
+;; URL: https://github.com/jcs090218/show-eol
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; Show end of line in buffer.
+;;
+
+;;; Code:
+
+
+
+(provide 'show-eol)
+;;; show-eol.el ends here
